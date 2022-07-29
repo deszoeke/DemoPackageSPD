2b2e5f6a396db3a6d045176e139724aa72cf7d78
Documenter.jl <documenter@juliadocs.github.io>
2022-07-29 02:32:05 +0000

    build based on 86d723c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageSPD","category":"page"},{"location":"#DemoPackageSPD","page":"Home","title":"DemoPackageSPD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageSPD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageSPD]","category":"page"}]
+}
